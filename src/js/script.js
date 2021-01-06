49bd2cea25f2ec7e71eeab9a1971bb7072f870f7
Andrei <Koleda05@gmail.com>
2021-01-06 18:49:09 +0100

    Created footer with google map and created modal window for consultation and order

--- src/js/script.js
+++ src/js/script.js
@@ -17,4 +17,82 @@ $(document).ready(function () {
      
     ],
   });
+
+  function toggleClassItem(link){
+    $(link).each( function(i){
+
+      $(this).on('click', function(e){
+        e.preventDefault();
+        $('.catalog-item__content').eq(i).toggleClass('catalog-item__content_active');
+        $('.catalog-item__list').eq(i).toggleClass('catalog-item__list_active');
+  
+      })
+    })
+  }
+
+
+//   const catalogTabs = document.querySelectorAll(".catalog__tab");
+// console.log(catalogTabs)
+//   catalogTabs.forEach( el => {
+//     el.addEventListener('click', function(e){
+//       el.classList.add("catalog__tab_active");
+//   });
+//   })
+   
+  $('ul.catalog__tabs').on('click', 'li:not(.catalog__tab_active)', function () {
+    $(this)
+      .addClass('catalog__tab_active').siblings().removeClass('catalog__tab_active')
+      .closest('div.container').find('div.catalog__content').removeClass('catalog__content_active').eq($(this).index()).addClass('catalog__content_active');
+  });
+
+
+  toggleClassItem('.catalog-item__link');
+  toggleClassItem('.catalog-item__back');
+
+
+  // $('.catalog-item__link').each( function(i){
+
+  //   $(this).on('click', function(e){
+  //     e.preventDefault();
+  //     $('.catalog-item__content').eq(i).toggleClass('catalog-item__content_active');
+  //     $('.catalog-item__list').eq(i).toggleClass('catalog-item__list_active');
+
+  //   })
+  // })
+
+
+  // $('.catalog-item__back').each( function(i){
+  //   $(this).on('click', function(e){
+  //     e.preventDefault();
+  //     $('.catalog-item__content').eq(i).toggleClass('catalog-item__content_active');
+  //     $('.catalog-item__list').eq(i).toggleClass('catalog-item__list_active');
+
+  //   })
+  // })
+
+
+  //Modal
+
+  $("[data-modal=consultation").on('click', function(){
+    $(".overlay, #consultation").fadeIn();
+  
+  });
+
+  $(".modal__close").on("click", function(){
+    $(".overlay,#consultation, #order, #thanks").fadeOut();
+ 
+  })
+
+  $(".button_tab").on('click', function(){
+    $(".overlay, #order").fadeIn();
+  
+  });
+
+  $('.button_tab').each( function(i){
+    $(this).on('click', function(){
+      $('#order .modal__descr').text($('.catalog-item__subtitle').eq(i).text());
+      
+    })
+  })
+
 });
